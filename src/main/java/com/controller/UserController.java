9faa831fd794937f97a0fde6cf5e9021beb9406d
Aakanksha Kumari <aakanksha.kumari@comviva.com>
2021-10-27 21:39:52 +0530

    Inital setup with user

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.service.UserService;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+}
